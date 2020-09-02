458355501cb00eae3251809da61898e7c1f74103
4leaf.ysh <4leaf.ysh@gmail.com>
2020-09-02 17:46:37 +0900

    before

--- src/api/Question/question/question.js
+++ src/api/Question/question/question.js
@@ -22,7 +22,22 @@ export default {
     getQuestionForTotal: async (_, args) => {
       const { round } = args;
 
-      // const avgArr = [];
+      try {
+        const result = await prisma.questions({
+          where: {
+            round,
+          },
+          orderBy: "sort_ASC",
+        });
+
+        return result;
+      } catch (e) {
+        console.log(e);
+        return false;
+      }
+    },
+    getAllQuestionForTotal: async (_, args) => {
+      const { round } = args;
 
       try {
         const result = await prisma.questions({
@@ -32,34 +47,10 @@ export default {
           orderBy: "sort_ASC",
         });
 
-        // await Promise.all(
-        //   result.map(async (data, index) => {
-        //     const EvaluationData = await prisma.evaluations({
-        //       where: {
-        //         question: {
-        //           id: data.id,
-        //         },
-        //       },
-        //     });
-
-        //     let sum = 0;
-
-        //     EvaluationData.map(async (doc, idx) => {
-        //       sum = sum + doc.score;
-
-        //       if (EvaluationData.length - 1 === idx) {
-        //         avgArr.push(sum / EvaluationData.length);
-        //       }
-        //     });
-        //   })
-        // );
-
-        // console.log(avgArr);
-
         return result;
       } catch (e) {
         console.log(e);
-        return false;
+        return [];
       }
     },
   },
